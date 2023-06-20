52d2ebeffbc9db43704cc400d8cc2d2f0f0fe98a
sripwoud <me@sripwoud.xyz>
2023-06-20 14:17:57 +0200

    Deploy to Mumbai

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-[`create-wagmi`](https://github.com/wagmi-dev/create-wagmi) `next-cli-foundry` template + solhint + linting + formatting + task caching.
+# NFT with updatable on chain metadata
+
+Adapter from [alchemy road to web3 tutorial](https://docs.alchemy.com/docs/how-to-make-nfts-with-on-chain-metadata-hardhat-and-javascript).
+
+## Deployment
+
+| Network | Contract Address                                                                                                                     |
+| ------- | ------------------------------------------------------------------------------------------------------------------------------------ |
+| Mumbai  | [0x92844a251EC890141F6617c9c2ef7eEb26a81ad8](https://mumbai.polygonscan.com/address/0x92844a251ec890141f6617c9c2ef7eeb26a81ad8#code) |
+
--- contracts/test/ChainBattles.t.sol
+++ contracts/test/ChainBattles.t.sol
@@ -15,7 +15,7 @@ contract PublicChainBattles is ChainBattles {
 contract ChainBattlesTest is Test, IERC721Receiver {
     PublicChainBattles public chainBattles;
 
-    // addresses that receives ERC721 tokens need to implement this interface
+    /// @dev addresses that receives ERC721 tokens need to implement this interface
     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data)
         external
         returns (bytes4)
@@ -63,6 +63,9 @@ contract ChainBattlesTest is Test, IERC721Receiver {
             "data:application/json;base64,eyJuYW1lIjogIkNoYWluIEJhdHRsZXMgIzAiLCJkZXNjcmlwdGlvbiI6ICJCYXR0bGVzIG9uIGNoYWluIiwiaW1hZ2UiOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNDhjM1I1YkdVK0xtSmhjMlVnZXlCbWFXeHNPaUIzYUdsMFpUc2dabTl1ZEMxbVlXMXBiSGs2SUhObGNtbG1PeUJtYjI1MExYTnBlbVU2SURFMGNIZzdJSDA4TDNOMGVXeGxQanh5WldOMElIZHBaSFJvUFNJeE1EQWxJaUJvWldsbmFIUTlJakV3TUNVaUlHWnBiR3c5SW1Kc1lXTnJJaUF2UGp4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTkRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1YyRnljbWx2Y2p3dmRHVjRkRDQ4ZEdWNGRDQjRQU0kxTUNVaUlIazlJalV3SlNJZ1kyeGhjM005SW1KaGMyVWlJR1J2YldsdVlXNTBMV0poYzJWc2FXNWxQU0p0YVdSa2JHVWlJSFJsZUhRdFlXNWphRzl5UFNKdGFXUmtiR1VpUGt4bGRtVnNjem9nTUR3dmRHVjRkRDQ4TDNOMlp6ND0ifQ=="
         );
         assertEq(chainBattles.getLevel(0), "0");
-        assertEq(chainBattles.generateCharacter(0), "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaW5ZTWluIG1lZXQiIHZpZXdCb3g9IjAgMCAzNTAgMzUwIj48c3R5bGU+LmJhc2UgeyBmaWxsOiB3aGl0ZTsgZm9udC1mYW1pbHk6IHNlcmlmOyBmb250LXNpemU6IDE0cHg7IH08L3N0eWxlPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9ImJsYWNrIiAvPjx0ZXh0IHg9IjUwJSIgeT0iNDAlIiBjbGFzcz0iYmFzZSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSI+V2FycmlvcjwvdGV4dD48dGV4dCB4PSI1MCUiIHk9IjUwJSIgY2xhc3M9ImJhc2UiIGRvbWluYW50LWJhc2VsaW5lPSJtaWRkbGUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkxldmVsczogMDwvdGV4dD48L3N2Zz4=");
+        assertEq(
+            chainBattles.generateCharacter(0),
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaW5ZTWluIG1lZXQiIHZpZXdCb3g9IjAgMCAzNTAgMzUwIj48c3R5bGU+LmJhc2UgeyBmaWxsOiB3aGl0ZTsgZm9udC1mYW1pbHk6IHNlcmlmOyBmb250LXNpemU6IDE0cHg7IH08L3N0eWxlPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9ImJsYWNrIiAvPjx0ZXh0IHg9IjUwJSIgeT0iNDAlIiBjbGFzcz0iYmFzZSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSI+V2FycmlvcjwvdGV4dD48dGV4dCB4PSI1MCUiIHk9IjUwJSIgY2xhc3M9ImJhc2UiIGRvbWluYW50LWJhc2VsaW5lPSJtaWRkbGUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkxldmVsczogMDwvdGV4dD48L3N2Zz4="
+        );
     }
 }
